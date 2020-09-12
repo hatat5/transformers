6701c50c09f68c27c37ec2c834b0e907aff4ab9a
hatat5 <hatat5@github.com>
2020-09-12 15:10:35 -0700

    added basic translation model

--- src/transformers/configuration_basic_tm.py
+++ src/transformers/configuration_basic_tm.py
@@ -9,7 +9,8 @@ class BasicTMEncoderConfig(PretrainedConfig):
     model_type = "basic_tm_encoder"
     def __init__(self,
                  encoder_params: Dict,
-                 tokenizer):
+                 tokenizer,
+                 **common_kwargs):
         super(BasicTMEncoderConfig, self).__init__(bos_token_id=tokenizer.bos_token_id,
                                             pad_token_id=tokenizer.pad_token_id,
                                             eos_token_id=tokenizer.eos_token_id)
@@ -22,7 +23,8 @@ class BasicTMDecoderConfig(PretrainedConfig):
     model_type = "basic_tm_decoder"
     def __init__(self,
                  decoder_params: Dict,
-                 tokenizer: GPT2Tokenizer):
+                 tokenizer: GPT2Tokenizer,
+                 **common_kwargs):
         super(BasicTMDecoderConfig, self).__init__(bos_token_id=tokenizer.bos_token_id,
                                                    pad_token_id=tokenizer.pad_token_id,
                                                    eos_token_id=tokenizer.eos_token_id,
