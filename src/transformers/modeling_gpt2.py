5d665df4c08840c323b718a8109704b7a2c74767
Nishant Subramani <nishant.subramani23@gmail.com>
2021-09-02 13:20:19 -0700

    added an inject first option

--- src/transformers/modeling_gpt2.py
+++ src/transformers/modeling_gpt2.py
@@ -519,7 +519,7 @@ class GPT2Model(GPT2PreTrainedModel):
             reshaped_projected_z_conditioning = projected_z_conditioning.repeat(1, seqlen, 1).requires_grad_(True)
         elif z_input_strategy == 'inject_first':
             zero_pad = torch.zeros_like(projected_z_conditioning).repeat(1, seqlen-1, 1)
-            reshaped_projected_z_conditioning = torch.cat((projected_z_conditioning, zero_pad), dim=1)
+            reshaped_projected_z_conditioning = torch.cat((projected_z_conditioning, zero_pad), dim=1).requires_grad_(True)
         else:
             raise ValueError('z_input_strategy needs to be inject, prompt or inject_first')
 
